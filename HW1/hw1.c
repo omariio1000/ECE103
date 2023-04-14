46415481a91e326319eca565253ef491ea0218d6
Omar Nassar <omariio1000@gmail.com>
2023-04-14 00:13:35 -0700

    started hw 1

---
+++ HW1/hw1.c
@@ -0,0 +1,24 @@
+/* Omar Nassar
+ * Portland State University ECE 103
+ * Homework 1
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#define BOLTZMANN 0.0000861735
+
+double getKelvin(double celsius) {
+    return celsius + 273.15;
+}
+
+int main (void) {
+    double oxidationTemp;
+    double initThickness;
+    double totalThickness;
+
+
+
+    return EXIT_SUCCESS;
+}
